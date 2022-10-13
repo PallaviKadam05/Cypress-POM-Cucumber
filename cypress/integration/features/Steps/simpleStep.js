34881148597362e34489280490d72ccf537e6a34
Pallavi Kadam <46708961+PallaviKadam05@users.noreply.github.com>
2022-10-13 15:43:12 +0530

    added file read write upload code

--- cypress/integration/features/Steps/simpleStep.js
+++ cypress/integration/features/Steps/simpleStep.js
@@ -54,3 +54,42 @@ Then('User overs the mouse on button to see list' ,value=>{
     cy.get('.mouse-hover button').trigger('mousemove').find('.mouse-hover .mouse-hover-content')
     .contains('Top','Reload')
 })
+
+Then('User visit file upload URL' ,()=>{
+    cy.forceVisit('https://www.filemail.com/share/upload-file')
+    cy.url().should('include','/share/upload-file')
+})
+
+Then('User upload file' ,()=>{
+    const file1 = 'Cypress-test-automation-framework.jpg'
+    const file2 = 'cypressVSselenium.jpeg'
+    cy.get("#addBtn [type='file']").attachFile([file1,file2])
+
+    cy.get("#addBtn [type='file']")
+    .attachFile({ filePath: file1, fileName: 'customFileName.json' });
+    cy.get('[data-name="customFileName.json"]').should('include.text', 'customFileName.json')
+})
+
+Then('Read the file by using fixture' ,()=>{
+    cy.fixture('example.json').then((data)=>{
+        console.log(data)
+        console.log(data.email)
+    })
+})
+
+Then('Read th file by using readfile' ,()=>{
+    cy.readFile('C:/Users/Pallavi/Documents/Cypress-POM-Cucumber/cypress/fixtures/example.json').then((data)=>{
+        console.log(data.email)
+    })
+})
+
+Then('Write the file' ,()=>{
+    cy.writeFile('C:/Users/Pallavi/Documents/Cypress-POM-Cucumber/cypress/fixtures/sample.txt', 'Hello I am Pallavi Kadam\n')
+    cy.writeFile('C:/Users/Pallavi/Documents/Cypress-POM-Cucumber/cypress/fixtures/sample.txt', 'From Sangli',{flag:'a+'})
+
+})
+
+Then('User download file', ()=>{
+    cy.downloadFile('https://upload.wikimedia.org/wikipedia/en/a/a9/Example.jpg','mydownloads','example.jpg','MyCustomAgentName')
+})
+
--- cypress/integration/features/simple.feature
+++ cypress/integration/features/simple.feature
@@ -2,24 +2,38 @@ Feature: Practice cypress
 Background: User should visit to paget
     Given User visits to the application
     
-    Scenario: User should able to see the page title
+    Scenario:[0] User should able to see the page title
     When User sees the title of the page "Automation Practice"
 
-    Scenario: User should able to select dropdown option
+    Scenario:[1] User should able to select dropdown option
     When User clicks on dropdown option
     Then User selects selenium option
 
-    Scenario: User should able to select radio option
+    Scenario:[2] User should able to select radio option
     When User clicks on "Radio1"
     Then User sees selected option "Radio1"
 
-    Scenario: User should able to select checkbox option
+    Scenario:[3] User should able to select checkbox option
     When User clicks on "option1" checkbox
     Then User sees selected checkbox "option1"
 
-    Scenario: User search India from dynamic input
+    Scenario:[4] User search India from dynamic input
     When User search "American Samoa" from dropdown
     Then User verifies searched value "American Samoa"
 
-    Scenario: User over the mouse to see list
+    Scenario:[5] User over the mouse to see list
     Then User overs the mouse on button to see list
+
+    Scenario:[6] File upload
+    Then User visit file upload URL
+    Then User upload file
+
+    Scenario:[6] File download
+    Then User download file
+@focus
+    Scenario:[7] Read files
+    Then Read the file by using fixture
+    Then Read th file by using readfile
+
+    Scenario:[8] Write files
+    Then Write the file
